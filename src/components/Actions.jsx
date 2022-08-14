203b0cf37a4991cbe4cc0bbde2daaa43f2b4c078
Josep <josepcortes6@gmail.com>
2022-08-14 16:53:44 +0200

    solved

---
+++ src/components/Actions.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Actions (){
+    <div className="actions">
+    <i class="far fa-comment"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="far fa-heart"></i>
+    <i class="fas fa-share"></i>
+  </div>
+}
---
+++ src/components/TImestamps.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function TimeStamps(props) {
+    return(
+        <span className="timestamp"> {props.time}</span>
+    )
+}
